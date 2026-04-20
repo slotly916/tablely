3db5b5a88032b9d5515489576ebcc979ab2676c5
slotly916 <twitchtock@gmail.com>
2026-04-20 20:22:46 +0200

    fix supbase url

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 import { Resend } from "resend";
 
 const supabase = createClient(
-  process.env.SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_KEY!
 );
 
